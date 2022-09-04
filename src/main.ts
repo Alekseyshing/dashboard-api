69b5f52fe0753bbfe3551d58c70dd1a1bff96da7
Aleksey <alekseytim@mail.ru>
2022-09-05 00:20:09 +0300

    feat: service users

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,15 @@ import { LoggerService } from "./logger/logger.service";
 import { TYPES } from "./types";
 import { UserController } from "./users/users.controller";
 import 'reflect-metadata';
+import { IUserService } from "./users/users.service.interface";
+import { IUserController } from "./users/users.controller.interface";
+import { UserService } from "./users/users.service";
 
 export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
   bind<ILogger>(TYPES.ILogger).to(LoggerService);
   bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
-  bind<UserController>(TYPES.UserController).to(UserController);
+  bind<IUserController>(TYPES.UserController).to(UserController);
+  bind<IUserService>(TYPES.UserService).to(UserService);
   bind<App>(TYPES.Application).to(App);
 }); 
 
---
+++ src/users/users.service.ts
@@ -0,0 +1,25 @@
+import { injectable } from "inversify";
+import { UserLoginDto } from "./dto/user-login.dto";
+import { UserRegisterDto } from "./dto/user-register.dto";
+import { User } from "./user.entity";
+import { IUserService } from "./users.service.interface";
+
+@injectable()
+export class UserService implements IUserService {
+  async createUser ({ email, name, password }: UserRegisterDto): Promise<User | null> {
+    const newUser = new User(email, name);
+    await newUser.setPassword(password);
+    //проверка что он есть
+    //если да то null 
+    //если нет - создаем
+    return null
+  }
+
+  async validateUser(dto: UserLoginDto): Promise<boolean> {
+    return true
+  };
+}
+
+function injecteble() {
+  throw new Error("Function not implemented.");
+}
