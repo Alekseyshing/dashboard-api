17730d8703995ae5ccf21ee7d71b8b804be77449
Aleksey <alekseytim@mail.ru>
2022-09-05 22:39:19 +0300

    feat: prisma

--- src/main.ts
+++ src/main.ts
@@ -12,12 +12,14 @@ import { IUserController } from "./users/users.controller.interface";
 import { UserService } from "./users/users.service";
 import { IConfigService } from "./config/config.service.interface";
 import { ConfigService } from "./config/config.service";
+import { PrismaService } from "./database/prisma.service";
 
 export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
   bind<ILogger>(TYPES.ILogger).to(LoggerService).inSingletonScope();
   bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
   bind<IUserController>(TYPES.UserController).to(UserController);
   bind<IUserService>(TYPES.UserService).to(UserService).inSingletonScope();
+  bind<PrismaService>(TYPES.PrismaService).to(PrismaService).inSingletonScope();
   bind<IConfigService>(TYPES.ConfigService).to(ConfigService).inSingletonScope();
   bind<App>(TYPES.Application).to(App);
 }); 
