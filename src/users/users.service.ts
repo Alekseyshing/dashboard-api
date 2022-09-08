8d1e5cc118a9f6e37cd139d04c10b19589331b90
Aleksey <alekseytim@mail.ru>
2022-09-08 23:03:33 +0300

    feat: users repo

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,22 +1,29 @@
+import { UserModel } from "@prisma/client";
 import { inject, injectable } from "inversify";
 import { IConfigService } from "../config/config.service.interface";
 import { TYPES } from "../types";
 import { UserLoginDto } from "./dto/user-login.dto";
 import { UserRegisterDto } from "./dto/user-register.dto";
 import { User } from "./user.entity";
+import { IUsersRepository } from "./users.repository.interface";
 import { IUserService } from "./users.service.interface";
 
 @injectable()
 export class UserService implements IUserService {
-  constructor(@inject(TYPES.ConfigService) private configService: IConfigService){}
-  async createUser ({ email, name, password }: UserRegisterDto): Promise<User | null> {
+  constructor(
+    @inject(TYPES.ConfigService) private configService: IConfigService,
+    @inject(TYPES.UsersRepository) private usersRepository: IUsersRepository
+  ){}
+  async createUser ({ email, name, password }: UserRegisterDto): Promise<UserModel | null> {
     const newUser = new User(email, name);
     const salt = this.configService.get('SALT');
     await newUser.setPassword(password, Number(salt));
-    //проверка что он есть
-    //если да то null 
-    //если нет - создаем
-    return null
+    const existedUser = await this.usersRepository.find(email);
+    if(existedUser){
+      return null
+    }
+
+    return this.usersRepository.create(newUser)
   }
 
   async validateUser(dto: UserLoginDto): Promise<boolean> {
@@ -24,6 +31,4 @@ export class UserService implements IUserService {
   };
 }
 
-function injecteble() {
-  throw new Error("Function not implemented.");
-}
+
