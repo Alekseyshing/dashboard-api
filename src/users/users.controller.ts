9c44d97bbe75cfa5638f1e2745de0177a2404809
Aleksey <alekseytim@mail.ru>
2022-09-19 13:49:52 +0300

    feat: middleware for JWT

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -20,28 +20,34 @@ export class UserController extends BaseController implements IUserController {
     @inject(TYPES.ILogger) private loggerService: ILogger,
     @inject(TYPES.UserService) private userService: IUserService,
     @inject(TYPES.ConfigService) private configService: IConfigService
-  ){
+  ) {
     super(loggerService);
     this.bindRoutes([
-      { 
-        path: '/register', 
-        method: 'post', 
-        func: this.register, 
-        middlewares: [new ValidateMiddleware(UserRegisterDto)] 
+      {
+        path: '/register',
+        method: 'post',
+        func: this.register,
+        middlewares: [new ValidateMiddleware(UserRegisterDto)]
       },
-      { 
-        path: '/login', 
-        method: 'post', 
-        func: this.login, 
-        middlewares: [new ValidateMiddleware(UserLoginDto)] 
+      {
+        path: '/login',
+        method: 'post',
+        func: this.login,
+        middlewares: [new ValidateMiddleware(UserLoginDto)]
+      },
+      {
+        path: '/info',
+        method: 'get',
+        func: this.info,
+        middlewares: []
       },
     ])
 
   }
 
-  async login(req: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction): Promise<void>{
+  async login(req: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction): Promise<void> {
     const result = await this.userService.validateUser(req.body);
-    if(!result){
+    if (!result) {
       return next(new HTTPError(401, 'Ошибка авторизации', 'login'));
     }
     const jwt = await this.signJWT(req.body.email, this.configService.get('SECRET'))
@@ -49,26 +55,30 @@ export class UserController extends BaseController implements IUserController {
   }
 
   async register(
-    { body }: Request<{}, {}, UserRegisterDto>, 
-    res: Response, 
+    { body }: Request<{}, {}, UserRegisterDto>,
+    res: Response,
     next: NextFunction
-    ): Promise<void>{
+  ): Promise<void> {
     const result = await this.userService.createUser(body);
-    if(!result){
+    if (!result) {
       return next(new HTTPError(422, 'Такой пользователь уже существует'))
     }
     this.ok(res, { email: result.email, id: result.id });
   }
 
-  private signJWT(email: string, secret: string): Promise<string>{
-    return new Promise<string> ((resolve, reject) => {
+  async info({ user }: Request, res: Response, next: NextFunction): Promise<void> {
+    this.ok(res, { email: user });
+  }
+
+  private signJWT(email: string, secret: string): Promise<string> {
+    return new Promise<string>((resolve, reject) => {
       sign({
         email,
         iat: Math.floor(Date.now() / 1000),
       }, secret, {
         algorithm: 'HS256',
       }, (err, token) => {
-        if(err){
+        if (err) {
           reject(err)
         }
         resolve(token as string);
